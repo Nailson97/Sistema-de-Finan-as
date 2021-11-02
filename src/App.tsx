596db4ae33030ed1a501d20b3e0a464da8b59a18
Nailson97 <nailsonsousa97@gmail.com>
2021-11-02 17:10:46 -0300

    iniciando projeto

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import * as C from './App.styles'
+
+export default function App () {
+  return(
+    <C.Container>
+        <C.Header>
+          <C.HeaderText>Sistema Financeiro</C.HeaderText>
+        </C.Header> 
+        <C.Body>
+           
+          {/* Area de Informações */}
+
+          {/* Area de inserção */}
+
+          {/* Tabela de itens */}
+
+        </C.Body>  
+    </C.Container>
+  )
+}
